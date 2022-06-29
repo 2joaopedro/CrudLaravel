9c7ac05a029bc6c6640aa18a0511000790a37f57
João <joao.pedro.franchini.4@gmail.com>
2022-06-29 15:08:16 -0300

    commit:PostController (functions crud)

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -1,85 +1,48 @@
 <?php
 
-namespace App\Http\Controllers;
-
 use App\Models\Post;
 use Illuminate\Http\Request;
 
-class PostController extends Controller
+class PostController extends Contrller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    //Exiba uma listagem do recurso.
+    public function index(){
+        $data = Post::latest()->paginate(5);
+
+        return view('post.index', compact('data'))
+        ->with('i',(request()->input('page',1) -1) * 5);
+    }
+    
+    //Mostre o formulário para criar um novo recurso.
+    public function create(){
+        return view('posts,create');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    //Armazene um recurso recém-criado no armazenamento.
+    public function store(Request $request){
+        $request ->validate(['title' => 'required','description'=> 'required',]);
+        Post::create($request->all());
+        return redirect()->route('posts.index')->with('sucess','Post crated successfully.');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    //Exibe o recurso especificado.
+    public function show(Post $post){
+        return view('posts.show',compact('post'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
+    //Atualize o recurso especificado no armazenamento.
+    public function update(Request $request, Post $post){
+        $request->validate(['title'=>'required','description'=>'required']);
+        $post->update($request->all());
+        return redirect()->route('posts.index')->with('sucess','Post updated successfully');
+    }
+    
+    //Remova o recurso especificado do armazenamento.
+    public function destroy(Post $post){
+        $post->delete();
+        
+        return redirect()->rout('posts.index')
+        ->with('success','Post deleted successfully');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
 }
