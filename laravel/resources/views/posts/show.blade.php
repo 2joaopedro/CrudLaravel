25a52f328b1460dbf227f0260b24cb5913e98af6
João <joao.pedro.franchini.4@gmail.com>
2022-06-30 16:50:07 -0300

    commit: alteracoes

--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -13,5 +13,17 @@
   </div>
 
   <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12"></div>
-  </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                {{ $post->title }} 
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $post->description }}
+            </div>
+        </div>
+    </div>
+@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,11 +6,10 @@ use App\Http\Controllers\TestController;
 
 // Route::resource('posts', PostController::class);
 Route::get('/', [TestController::class, 'home']);
-
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('posts/store', [PostController::class, 'store'])->name('posts.store');
 Route::get('posts/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::delete('posts/destroy', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('posts/show', [PostController::class, 'show'])->name('posts.show');
+Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::put('posts/update', [PostController::class, 'update'])->name('posts.update');
